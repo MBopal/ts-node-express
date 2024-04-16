4b937558c993a71508db89a3ff9f3175665dcd4c
Muhammad Naufal <naufalmhd24@gmail.com>
2024-04-16 17:23:18 +0700

    fix public and private key path

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,8 +1,8 @@
 import 'dotenv/config'
 import fs from 'fs'
 
-const privateKey = fs.readFileSync('src/privateKey.pem', 'utf8')
-const publicKey = fs.readFileSync('src/publicKey.pem', 'utf8')
+const privateKey = fs.readFileSync('../privateKey.pem', 'utf8')
+const publicKey = fs.readFileSync('../publicKey.pem', 'utf8')
 
 const CONFIG = {
   db: process.env.MONGO_URI,
